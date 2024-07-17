670628a5cb57bcdefd1aa59ba849c2d3c7841631
Beshoyyoussefrezk <beshoyyoussef34@gmail.com>
2024-07-17 10:23:06 +0300

    edit3 commit

--- src/index.js
+++ src/index.js
@@ -6,16 +6,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4000;
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,7 +15,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
